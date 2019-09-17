dad57a9d49350e507d832669c69242f110d5285c
Sean Cranford <sean99@ksu.edu>
2019-09-17 01:10:32 -0500

    Did first part of count files

---
+++ count_files_and_subdirs.sh
@@ -0,0 +1,8 @@
+# this program counts the number of files and subdirectories in your working directory
+
+eout=`ls -1 | wc -l`
+
+echo "there are $eout files and subdirectories in the current directory"
+
+
+
